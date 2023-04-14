91fa4488e24afe08bc8a2eab8eca04401376a876
Diskantis <mn.zajkov@gmail.com>
2023-04-14 17:15:04 +0300

    solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,23 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample(sampleActivity) {
+  let years = 0;
+  if(typeof sampleActivity !== "string") {
+    return false
+  }
+  let num = Number(sampleActivity)
+  if(MODERN_ACTIVITY < num || num <= 0 || isNaN(num) || num === null) {
+    return false
+  } else {
+    years = Math.log(MODERN_ACTIVITY/num)/(0.693/HALF_LIFE_PERIOD)
+  }
+  if(Math.floor(years) > 0) {
+    return Math.ceil(years)
+  } else {
+    return false
+  }
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date = 'no-date') {
+  let seas = '';
+
+  if (date === 'no-date') {
+    return 'Unable to determine the time of year!'
+  // } else if () {
+  //   throw new Error('Invalid date!" on tricky moment');
+  // } else if () {
+  //   throw new Error('Invalid date!" on a very tricky moment');
+  } else if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw new Error('Invalid date!" on a very tricky moment');
+  } else if (Date.parse(date)){
+    const season = ['winter', 'spring', 'summer', 'autumn']
+    let month = date.getMonth()
+
+    if(month === 11 || month === 0 || month === 1){
+      seas += season[0] //'winter'
+    } else if (month === 2 || month === 3 || month === 4){
+      seas += season[1] //'spring'
+    } else if (month === 5 || month === 6 || month === 7){
+      seas += season[2] //'summer'
+    } else if (month === 8 || month === 9 || month === 10){
+      seas += season[3] //'autumn'
+    }
+    return seas;
+  } else {
+    return false
+  }
 }
 
 module.exports = {
